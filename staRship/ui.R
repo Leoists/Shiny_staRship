27e272bcdf9fa5d1bce8640b6f2c10dd615138a6
Alex Bokov <bokov@uthscsa.edu>
2023-01-31 16:54:56 -0600

    multi; first commit; most of the UI placeholders

---
+++ staRship/ui.R
@@ -0,0 +1,60 @@
+library(shiny);
+library(rgl);
+library(DT);
+library(bslib);
+
+ui <- fluidPage(
+  theme = bs_theme(primary = "#2E302E", base_font = font_google("Share Tech"), 
+                   code_font = font_google("Share Tech Mono"), heading_font = font_google("Geostar Fill"), 
+                   font_scale = NULL, `enable-transitions` = FALSE, spacer = "0.3rem", 
+                   bootswatch = "cyborg"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "starship.css")
+  ),
+  
+  # Narrow banner with logo?
+  titlePanel(span(img(src = "starship_logo_placeholder.jpg", height = 50, align = "left")
+                  ,HTML('&emsp;')
+                  ,tags$small("staRship: the open source retro space web game"))
+             ,windowTitle='StaRship'
+  ),
+  
+  # Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      # Table of ship statistics
+      "Ship Status",
+      DT::dataTableOutput("ship_stats"),
+      tags$img(src = "starship_diagram_placeholder.jpg", width = "200px"),
+      
+      # Scrollable div
+      tags$div(id = "scrollable", 
+               style = "overflow-y: scroll; height: 300px;",
+               "Content goes here..."
+      ),
+      
+      # Debug button
+      if(file.exists('.debug')){actionButton("debug","Debug")},
+      width=2
+    ),
+    
+    # Main content area
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Starmap", 
+                 rglwidgetOutput("starmap"),
+                 DT::dataTableOutput("destinations"),
+                 actionButton("continue", "Continue to current destination"),
+                 actionButton("new_destination", "Choose new destination"),
+                 actionButton("probes", "Send probes"),
+                 actionButton("colony", "Start colony")
+        ),
+        tabPanel("Ship Log", DT::dataTableOutput("ship_log")),
+        tabPanel("Event"
+                 ,img(src="event_example.gif",width="800vw")
+                 , tags$div(id = "event_output")),
+        tabPanel("About", tags$div(id = "about_output"))
+      )
+    )
+  )
+)
