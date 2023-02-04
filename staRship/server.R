20870fa3991e7caca87e8442bca1714196dc9ef8
Alex Bokov <bokov@uthscsa.edu>
2023-02-03 22:25:29 -0600

    server.R; feature; We have a real server function now.

--- staRship/server.R
+++ staRship/server.R
@@ -12,17 +12,26 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+  output$starmap <- renderRglwidget({
+    clear3d()
+    #dat <- iris[, c(input$plot_x, input$plot_y, input$plot_z, "Species")]
+    #dat$id <-as.character(seq_len(nrow(iris)))
+    #@rglids <<- ;
+    #plot3d(x = dat[, 1:3], type = "s", size = 1, col = as.integer(iris[, "Species"]), aspect = TRUE)
+    sharedData <<- rglShared(id = rglStarChart(foo)['data']) #rglids['data'])
+                             # text3d(dat[, 1:3], text = dat[, "id"], adj = -0.5),
+                             # group = "SharedData_plot_3D_ids",
+                             # deselectedFade = 0,
+                             # selectedIgnoreNone = FALSE)
+    rglwidget(shared=sharedData);
+    # shinyResetBrush(session, "rgl_3D_brush")
+    # rglwidget(shared = sharedData,
+    #           shinyBrush = "rgl_3D_brush")
+  });
+  
+  output$event_output <- renderText("The seedship's course takes it through a dense star-forming nebula. Hundreds of infant suns illuminate clouds of interstellar gas and fill the sky with riotous color. Clouds twist through complex gravitational interference patterns and glitter with heavy elements formed in the death throes of the last generation of stars. \n\nThe scanners were not designed to deal with this level of input, and it is threatening to overwhelm them. If the AI continues to use the scanners to navigate, it can tell that the gravity_scanner will be damaged. Shutting off the scanners, however, would leave the seedship vulnerable to collisions in this crowded area of space.");
+  
+  output$ship_log <- DT::renderDataTable({
+    datatable(foo$log[,c('stardate','description')],rownames = F,style = 'bootstrap')});
+  
 })
