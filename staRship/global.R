1ea4ac45d88750d9ff0ba0ae0bf2f2138e09c77b
Alex Bokov <bokov@uthscsa.edu>
2023-02-03 22:24:21 -0600

    global.R; init; Loads some libraries and sets datatable options.

---
+++ staRship/global.R
@@ -0,0 +1,12 @@
+library(rgl);
+library(DT);
+library(crosstalk);
+library(dplyr);
+library(stringr);
+
+options(datatable.na.strings=c('NULL','','NA'));
+options(DT.options=list(dom='Bfrtip',paging=F,scrollY='70vh' #'500px'
+                        ,scrollX=TRUE #'20vw' #'100px'
+                        ,pageLength= -1,class=c('display','compact','order-column')));
+load('demo_data.rdata');
+
