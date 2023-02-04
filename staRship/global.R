698544e461db31ea7c000ef3da9630dd1018999c
Alex Bokov <bokov@uthscsa.edu>
2023-02-04 15:40:21 -0600

    global.R; features; Sources functions.R after loading demo_data so that they overwrite the older/buggier functions in the demo_data.

--- staRship/global.R
+++ staRship/global.R
@@ -4,9 +4,11 @@ library(crosstalk);
 library(dplyr);
 library(stringr);
 
+
 options(datatable.na.strings=c('NULL','','NA'));
 options(DT.options=list(dom='Bfrtip',paging=F,scrollY='70vh' #'500px'
                         ,scrollX=TRUE #'20vw' #'100px'
                         ,pageLength= -1,class=c('display','compact','order-column')));
 load('demo_data.rdata');
-
+source('functions.R');
+NULL
