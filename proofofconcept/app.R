5c9a9827574b792382d9e0d5c3275e148ddaf6ad
Alex Bokov <bokov@uthscsa.edu>
2023-06-07 13:36:18 -0500

    proofofconcept/*; bugfix; Fixes the broken images (and, as it turns out, CSS) problem.

--- proofofconcept/app.R
+++ proofofconcept/app.R
@@ -15,8 +15,7 @@ library(dplyr);
 li <- tags$li
 ul <- tags$ul 
 
-
-parralaxFiles <- list.files("www/images",pattern = ".png",full.names = TRUE)
+parralaxFiles <- list.files("www",pattern = ".png",full.names = FALSE,recursive=TRUE)
 # Define UI for application that draws a histogram
 ui <- fillPage(
   theme = bs_theme(primary = "#2E302E", base_font = font_google("Share Tech"),
@@ -32,24 +31,30 @@ ui <- fillPage(
          ,parralaxFiles
          ,seq(0.05,0.4,along.with = parralaxFiles)
          ,SIMPLIFY = FALSE) %>% ul(id = "parallax-scene"),
-  img(src="./www/images/stars00.png")
-  
+  tags$script(HTML( 
+        "
+        var scene = document.getElementById('parallax-scene');
+        var parallaxInstance = new Parallax(scene, {
+            relativeInput: true
+        });
+
+        // get all the image elements inside .layer
+        var images = document.querySelectorAll('.layer img');
+        
+        // loop over each image and set its scale
+        for (var i = 0; i < images.length; i++) {
+            var scale = 1 + i * 0.5; 
+            // this will give 1.0 for the first image, 1.1 for the second, 1.2 for the third, etc.
+            images[i].style.transform = 'scale(' + scale + ')';
+        }
+        "
+    ))
 
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
 }
 
 # Run the application 
